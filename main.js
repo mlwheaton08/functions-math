3f7a4dbd5a728ea08b4399f8746192aa919467b2
mlwheaton08 <mlwheaton06@gmail.com>
2022-08-16 19:57:57 -0500

    first commit

---
+++ main.js
@@ -0,0 +1,12 @@
+const addTwoNumbers = (num1, num2) => {
+    return num1 + num2;
+}
+
+const multiplyByTwo = (num) => {
+    return num * 2;
+}
+
+const x = addTwoNumbers(3, 4);
+const multiplyXByTwo = multiplyByTwo(x);
+
+console.log(multiplyXByTwo);
